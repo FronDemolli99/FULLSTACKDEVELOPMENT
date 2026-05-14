fc3adf5477f4cd4cbd26b53616e7da4041f387f2
FronDemolli99 <frondemolli@gmail.com>
2026-05-14 20:45:23 +0200

    Module26

--- Module26/js/main.js
+++ Module26/js/main.js
@@ -1,7 +1,82 @@
-console.log("Goodbye")
+ // console.log("Goodbye");
 
-var x = 10;
+ // var x = 10;
 
-var _value ="digital school"
+// // var _value ="digital school";
 
-// var 123 = 55: wrong way //
+// // // var 123 = 55: wrong way //
+
+// // var a = 50+50;
+
+// // console.log(a);
+
+// // var b = 20;
+// // var c= 30;
+// // var d = b + c ;
+// // console.log(d);
+
+// // var s = 33;
+
+// // var x = 44;
+
+// // var w = s * x;
+
+// // console.log(w);
+
+// // var a = 99;
+
+// // a++;
+
+// // console.log(a)
+
+// // a--;
+
+// // console.log(a)
+
+
+
+// // var x = 15;
+
+// // x+=5;
+
+// // console.log(x)
+
+// // var x = 5;
+// // var y = 5;
+
+// // // console.log(x == y);
+
+// // console.log(x != y)
+
+// // var firstName = "Fron"
+
+// // var lastName = "DEMOLLI"
+
+// // var fillName = "firstName + "" + lastName"
+
+// // console.log(fullName);
+
+// // var school = "Digital School"
+// // console.log("Hello from" $(school))
+
+// var age = 67;
+
+// console.log(age >= 18 && age <= 65):
+
+// var var1 = 5;
+// var var2 = 6;
+
+// console.log(var1 == 5 || var2 == 5);
+
+// var var3 = 9
+// var var4 = 10
+
+// console.log(!(var3 == var4));
+
+
+var a = 5;
+var b = 6;
+
+var c = a + b;
+
+console.log(`the resault is $(c)`);
