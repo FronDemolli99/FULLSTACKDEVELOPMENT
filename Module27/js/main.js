7129ffced2a454de23a38bb77d4f3a83e1f16b56
FronDemolli99 <frondemolli@gmail.com>
2026-05-19 20:40:28 +0200

    module27

---
+++ Module27/js/main.js
@@ -0,0 +1,34 @@
+// function showMessage() {
+   
+//     alert("This is my first function ")
+
+// }
+
+// showMessage()
+
+// function sum(number1, number2) {
+//     return number1 + number2;
+// }
+
+// console.log(sum(32,35))
+
+
+// function toCelsius(fharenhight){
+//     return (5/9) * (fharenhight - 32)
+// }
+
+
+// var resault = toCelsius(54);
+
+// console.log("Faranhite in celcius is"+ reault + "degrees");
+
+
+function calculator(nr1, nr2) {
+    console.log (nr1 + nr2 );
+    console.log (nr1 - nr2 );
+    console.log (nr1 / nr2 );
+    console.log (nr1 * nr2 );
+    
+}
+
+calculator(77,11)
